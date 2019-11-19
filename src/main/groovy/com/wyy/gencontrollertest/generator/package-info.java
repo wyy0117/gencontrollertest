cb3ed19b78506e079a20fd55e4ed338e6b0bcd2a
wyy0117 <349093330@qq.com>
2019-11-19 21:25:05 +0800

    github的package repository需要认证才能install，放到maven中央仓库中

--- src/main/groovy/com/wyy/gencontrollertest/generator/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 定义了所有的代码生成器
- *
- * @Date: 19-11-2
- * @Author: wyy
- */
-package com.wyy.gencontrollertest.generator;
--- src/main/groovy/com/wyy/gencontrollertest/reader/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 定义了对controller类的解析
- *
- * @Date: 19-11-2
- * @Author: wyy
- */
-package com.wyy.gencontrollertest.reader;
